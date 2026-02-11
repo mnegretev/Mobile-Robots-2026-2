1d741ae47533cc88ffb886e8b3622763ab0f2de8
AlanCarrasco <alancarrasco2401@gmail.com>
2026-02-11 00:50:43 -0600

    Cambios para la tarea 02 y tratando de corregir un segmentation fault del MuJoCo

--- ros2_ws/src/hardware/humanoid_simul/src/basic_mujoco.cpp
+++ ros2_ws/src/hardware/humanoid_simul/src/basic_mujoco.cpp
@@ -104,16 +104,16 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset) {
 int main(int argc, const char** argv) {
   // check command-line arguments
   if (argc!=2) {
-    std::printf(" USAGE:  basic modelfile\n");
+    std::printf(" USAGE: basic modelfile\n");
     return EXIT_FAILURE;
   }
 
   // load and compile model
   char error[1000] = "Could not load binary model";
-  if (std::strlen(argv[1])>4 && !std::strcmp(argv[1]+std::strlen(argv[1])-4, ".mjb")) {
+  if (std::strlen(argv[1])>4 && !std::strcmp(argv[1]+std::strlen(argv[1])-4, ".mjb")){
     m = mj_loadModel(argv[1], 0);
   } else {
-    m = mj_loadXML(argv[1], 0, error, 1000);
+     m = mj_loadXML(argv[1], 0, error, 1000);
   }
   if (!m) {
     mju_error("Load model error: %s", error);
