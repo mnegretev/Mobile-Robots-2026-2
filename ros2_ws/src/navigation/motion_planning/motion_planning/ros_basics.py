52e9e3e63c9fc32250920f803a6d05c22124816b
Zambrano Miranda Isaac Jaciel <isaacjaczam@gmail.com>
2026-02-20 01:14:08 -0600

    Tarea 02 robots moviles - obstacle detection y testing point

--- ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
+++ ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
@@ -1,4 +1,4 @@
-#
+
 # MOBILE ROBOTS - FI-UNAM, 2026-2
 # THE PLATFORM ROS 
 #
@@ -13,7 +13,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist, PointStamped
 from sensor_msgs.msg import LaserScan
 
-FULL_NAME = "FULL NAME"
+FULL_NAME = "Zambrano Miranda Isaac Jaciel"
 
 class RosBasicsNode(Node):
     def __init__(self):
@@ -24,7 +24,6 @@ class RosBasicsNode(Node):
         self.sub_scan = self.create_subscription(LaserScan, '/scan', self.callback_scan, 1)
         self.timer = self.create_timer(0.1, self.callback_timer)
         self.obstacle_detected = False
-
     def callback_timer(self):
         #
         # TODO:
@@ -34,6 +33,19 @@ class RosBasicsNode(Node):
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         # Publish a point stamped with fixed coordinates (1,0)
         #
+        msg_twist = Twist()
+        msg_twist.linear.x = 0.0 if self.obstacle_detected else 0.3
+        self.pub_cmd_vel.publish(msg_twist)
+
+        msg_point = PointStamped()
+        msg_point.header.frame_id = "base_link"
+        msg_point.header.stamp = self.get_clock().now().to_msg()
+        msg_point.point.x = 1.0
+        msg_point.point.y = 0.0
+             
+        self.pub_point.publish(msg_point)
+
+
         
         return
 
@@ -43,6 +55,11 @@ class RosBasicsNode(Node):
         # Do something to detect if there is an obstacle in front of the robot.
         # Set the 'obstacle_detected' variable with True or False, accordingly.
         #
+        self.obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
+
+        msg_twist = Twist()
+        msg_twist.linear.x = 0.0 if self.obstacle_detected else 0.3
+        self.pub_cmd_vel.publish(msg_twist)
         
         return
 
