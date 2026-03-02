ca8e2376368f46eb937c0632ccdf9000fa0dac3e
hectorsaul472 <hectorsaul472@gmail.com>
2026-03-01 23:08:40 -0600

    Practica 1

--- ros2_ws/src/navigation/path_planner/path_planner/a_star.py
+++ ros2_ws/src/navigation/path_planner/path_planner/a_star.py
@@ -19,7 +19,7 @@ import numpy
 import heapq
 import math
 
-NAME = "FULL NAME"
+NAME = "Hector Saul Hernandez Saldivar"
 
 class AStarNode(Node):
     def a_star(self, start_r, start_c, goal_r, goal_c, grid_map, cost_map, use_diagonals):
@@ -66,6 +66,28 @@ class AStarNode(Node):
         #
         # END OF WHILE
         #
+        while len(open_list)>0 and (row!=goal_r or col!=goal_c):
+            current_f,current_node=heapq.heappop(open_list)
+            row,col=current_node[0],current_node[1]
+            in_closed_list[row, col]=True
+            for adj in adjacents:
+                r=row +adj[0]
+                c= col+adj[1]
+                dist=adj[2]
+                if r<0 or r>=height or c<0 or c>=width:
+                    continue
+                if grid_map[r,c]>50 or grid_map[r,c]==-1 or in_closed_list[r,c]:
+                    continue
+                g=g_values[row,col]+dist+ cost_map[r,c]
+                h=math.hypot(goal_r-r,goal_c-c)
+                f=g+h
+                if g<g_values[r,c]:
+                    g_values[r,c]=g
+                    f_values[r,c]=f
+                    parent_nodes[r,c]=[row,col]
+                    if not in_open_list[r,c]:
+                        in_open_list[r,c]=True
+                        heapq.heappush(open_list,(f,[r,c]))
         path = []
         while parent_nodes[goal_r, goal_c][0] != -1:
             path.insert(0, [goal_r, goal_c])
