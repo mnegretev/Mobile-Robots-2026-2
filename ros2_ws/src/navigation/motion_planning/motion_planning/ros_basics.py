941ee55a6c1b944434fdd38fcb442fe972ae5923
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2026-03-12 14:38:51 -0600

    Update files for path tracking

--- ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
+++ ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
@@ -1,6 +1,6 @@
 #
 # MOBILE ROBOTS - FI-UNAM, 2026-2
-# THE PLATFORM ROS
+# THE PLATFORM ROS 
 #
 # Instructions:
 # Write a program to move the robot forwards until the laser
@@ -8,25 +8,20 @@
 # Also, publish a point stamped with fixed coordinates
 # Required publishers and subscribers are already declared and initialized.
 
-from typing import Self
-
 import rclpy
-from geometry_msgs.msg import PointStamped, Twist
 from rclpy.node import Node
+from geometry_msgs.msg import Twist, PointStamped
 from sensor_msgs.msg import LaserScan
 
-FULL_NAME = "SANTIAGO CRUZ PLAZA"
-
+FULL_NAME = "FULL NAME"
 
 class RosBasicsNode(Node):
     def __init__(self):
         print("INITIALIZING ROS BASICS NODE - ", FULL_NAME)
         super().__init__("ros_basics_node")
-        self.pub_cmd_vel = self.create_publisher(Twist, "/cmd_vel", 1)
-        self.pub_point = self.create_publisher(PointStamped, "/testing_point", 1)
-        self.sub_scan = self.create_subscription(
-            LaserScan, "/scan", self.callback_scan, 1
-        )
+        self.pub_cmd_vel = self.create_publisher(Twist, '/cmd_vel', 1)
+        self.pub_point   = self.create_publisher(PointStamped, '/testing_point', 1)
+        self.sub_scan = self.create_subscription(LaserScan, '/scan', self.callback_scan, 1)
         self.timer = self.create_timer(0.1, self.callback_timer)
         self.obstacle_detected = False
 
@@ -35,17 +30,11 @@ class RosBasicsNode(Node):
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
-        # Use the 'obstacle_detected' variable to check if there is an obstacle.
+        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         # Publish a point stamped with fixed coordinates (1,0)
         #
-        msg_twist = Twist()
-        msg_twist.linear.x = 0.0 if self.obstacle_detected else 0.3
-        self.pub_cmd_vel.publish(msg_twist)
-        msg_point = PointStamped()
-        msg_point.header.frame_id = "base_link"
-        msg_point.point.x = 1.0
-        self.pub_point.publish(msg_point)
+        
         return
 
     def callback_scan(self, msg):
@@ -54,7 +43,7 @@ class RosBasicsNode(Node):
         # Do something to detect if there is an obstacle in front of the robot.
         # Set the 'obstacle_detected' variable with True or False, accordingly.
         #
-        self.obstacle_detected = msg.ranges[len(msg.ranges) // 2] < 1.0
+        
         return
 
 
@@ -65,6 +54,5 @@ def main(args=None):
     ros_basics_node.destroy_node()
     rclpy.shutdown()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
