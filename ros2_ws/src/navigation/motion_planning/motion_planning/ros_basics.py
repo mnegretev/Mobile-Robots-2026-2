dc89dd6460d0c525e97ecd026db8b072650d37a4
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2026-03-12 14:39:27 -0600

    Update files for path tracking

--- ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
+++ ros2_ws/src/navigation/motion_planning/motion_planning/ros_basics.py
@@ -1,9 +1,19 @@
+#
+# MOBILE ROBOTS - FI-UNAM, 2026-2
+# THE PLATFORM ROS 
+#
+# Instructions:
+# Write a program to move the robot forwards until the laser
+# detects an obstacle in front of it.
+# Also, publish a point stamped with fixed coordinates
+# Required publishers and subscribers are already declared and initialized.
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, PointStamped
 from sensor_msgs.msg import LaserScan
 
-FULL_NAME = "OSCAR SALDIVAR PANTOJA"
+FULL_NAME = "FULL NAME"
 
 class RosBasicsNode(Node):
     def __init__(self):
@@ -16,22 +26,26 @@ class RosBasicsNode(Node):
         self.obstacle_detected = False
 
     def callback_timer(self):
-        # Lógica de movimiento (Actividad 2)
-        msg_twist = Twist()
-        msg_twist.linear.x = 0.0 if self.obstacle_detected else 0.3
-        self.pub_cmd_vel.publish(msg_twist)
-
-        # Publicación del punto (Actividad 7)
-        msg_point = PointStamped()
-        msg_point.header.frame_id = "base_link"
-        msg_point.point.x = 1.0
-        self.pub_point.publish(msg_point)
+        #
+        # TODO:
+        # Declare a Twist message and assign the appropiate speeds:
+        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
+        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
+        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
+        # Publish a point stamped with fixed coordinates (1,0)
+        #
+        
         return
 
     def callback_scan(self, msg):
-        # Detección de obstáculos (Actividad 2)
-        self.obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
+        #
+        # TODO:
+        # Do something to detect if there is an obstacle in front of the robot.
+        # Set the 'obstacle_detected' variable with True or False, accordingly.
+        #
+        
         return
+
 
 def main(args=None):
     rclpy.init(args=args)
