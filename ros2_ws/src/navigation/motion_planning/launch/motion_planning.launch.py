c091201efe63a555daece1475e2b5f0008628eaa
Marceline Palomares <mar.palomaresq@gmail.com>
2026-03-16 01:46:44 -0600

    Tarea 07

--- ros2_ws/src/navigation/motion_planning/launch/motion_planning.launch.py
+++ ros2_ws/src/navigation/motion_planning/launch/motion_planning.launch.py
@@ -86,7 +86,7 @@ def generate_launch_description():
         executable="pure_pursuit",
         name='pure_pursuit',
         output='screen',
-        parameters=[{'alpha':0.1}, {'beta':0.1}]
+        parameters=[{'alpha':0.5}, {'beta':0.3}]
     )
     stanley_node = Node(
         package="path_follower",
@@ -110,6 +110,6 @@ def generate_launch_description():
         cost_map_node,
         a_star_node,
         path_smoothing_node,
-        #pure_pursuit_node
+        pure_pursuit_node
         #stanley_node
     ])
