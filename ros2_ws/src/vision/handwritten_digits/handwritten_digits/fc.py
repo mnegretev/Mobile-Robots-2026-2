990fa3ba97c59e8d18c416d505557c8876e4b8ec
Zambrano Miranda Isaac Jaciel <isaacjaczam@gmail.com>
2026-05-05 12:28:03 -0600

    Actividad 5: load_dataset y main con soporte binario, nombre en linea 15

--- ros2_ws/src/vision/handwritten_digits/handwritten_digits/fc.py
+++ ros2_ws/src/vision/handwritten_digits/handwritten_digits/fc.py
@@ -11,17 +11,11 @@ import sys
 import random
 import numpy
 import os
+
 NAME = "Isaac Jaciel Zambrano Miranda"
 
 class FCNeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
-        #
-        # The list 'layers' indicates the number of neurons in each layer.
-        # Remember that the first layer indicates the input dimension. 
-        # All weights and biases are initialized with random values. In each layer (except the first one)
-        # we have a matrix of weights where row j contains all the weights of the j-th neuron in that layer,
-        # and a vector of biases.
-        #
         self.biases =[numpy.random.randn(y,1) for y in layers[1:]] if biases == None else biases
         self.weights=[numpy.random.randn(y,x) for x,y in zip(layers[:-1],layers[1:])] if weights==None else weights
 
@@ -38,28 +32,17 @@ class FCNeuralNetwork(object):
         y = self.feedforward(x)
         nabla_b = [numpy.zeros(b.shape) for b in self.biases]
         nabla_w = [numpy.zeros(w.shape) for w in self.weights]
-
-        # Delta capa de salida
         delta = (y[-1] - t) * y[-1] * (1 - y[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = numpy.dot(delta, y[-2].T)
-
-        # Capas ocultas (desde la penultima hacia atras)
         L = len(self.weights)
         for i in range(2, L + 1):
             delta = numpy.dot(self.weights[-i + 1].T, delta) * y[-i] * (1 - y[-i])
             nabla_b[-i] = delta
             nabla_w[-i] = numpy.dot(delta, y[-i - 1].T)
-
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
-        #
-        # This function exectutes gradient descend for the subset of examples
-        # given by 'batch' with learning rate 'eta'
-        # 'batch' is a list of training examples [(x,t), ..., (x,t)]
-        # Function returns the magnitude of calculated gradient 
-        #
         batch_nabla_b = [numpy.zeros(b.shape) for b in self.biases]
         batch_nabla_w = [numpy.zeros(w.shape) for w in self.weights]
         M = len(batch)
@@ -76,9 +59,6 @@ class FCNeuralNetwork(object):
         return mag_nabla
 
     def train_by_SGD(self, training_x, training_t, epochs, batch_size, eta):
-        #
-        # This function implements the Stochastic Gradient Descend
-        #
         training_data = list(zip(training_x, training_t))
         for j in range(epochs):
             random.shuffle(training_data)
@@ -88,21 +68,29 @@ class FCNeuralNetwork(object):
                 sys.stdout.write("\rGradient magnitude: %f            " % (nabla_magnitude))
                 sys.stdout.flush()
             print("Epoch: " + str(j))
-    #
     ### END OF CLASS
-    #
 
-def load_dataset(folder):
+def load_dataset(folder, binary=False):
     print("Loading data set from " + folder)
     training_x, training_t, testing_x, testing_t = [],[],[],[]
+
+    # Etiquetas one-hot (10 clases) -- Actividades 3 y 4
     labels = [[1,0,0,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0,0,0], [0,0,1,0,0,0,0,0,0,0],
               [0,0,0,1,0,0,0,0,0,0], [0,0,0,0,1,0,0,0,0,0], [0,0,0,0,0,1,0,0,0,0],
               [0,0,0,0,0,0,1,0,0,0], [0,0,0,0,0,0,0,1,0,0], [0,0,0,0,0,0,0,0,1,0],
               [0,0,0,0,0,0,0,0,0,1]]
+    label_size = 10
+
+    # Etiquetas en codigo binario de 4 bits -- Actividad 5
+    if binary:
+        labels = [[0,0,0,0], [0,0,0,1], [0,0,1,0], [0,0,1,1], [0,1,0,0],
+                  [0,1,0,1], [0,1,1,0], [0,1,1,1], [1,0,0,0], [1,0,0,1]]
+        label_size = 4
+
     for i in range(10):
         f_data = [c/255.0 for c in open(os.path.join(folder, "data" + str(i)), "rb").read(784000)]
         images = [numpy.asarray(f_data[784*j:784*(j+1)]).reshape([784,1]) for j in range(1000)]
-        label  = numpy.asarray(labels[i]).reshape([10,1])
+        label  = numpy.asarray(labels[i]).reshape([label_size, 1])
         training_x += images[0:len(images)//2]
         training_t += [label for j in range(len(images)//2)]
         testing_x  += images[len(images)//2:len(images)]
@@ -116,8 +104,18 @@ def main(args=None):
     epochs        = 3
     batch_size    = 50
     learning_rate = 1.0
-    training_x, training_t, testing_x, testing_t = load_dataset(dataset_folder)
-    nn = FCNeuralNetwork([784, 30, 10])
+
+    # Cambiar a True para usar etiquetas binarias de 4 bits (Actividad 5)
+    use_binary = False
+
+    training_x, training_t, testing_x, testing_t = load_dataset(dataset_folder, binary=use_binary)
+
+    # Arquitectura one-hot: [784, 30, 10]  |  Arquitectura binaria: [784, 30, 4]
+    if use_binary:
+        nn = FCNeuralNetwork([784, 30, 4])
+    else:
+        nn = FCNeuralNetwork([784, 30, 10])
+
     nn.train_by_SGD(training_x, training_t, epochs, batch_size, learning_rate)
     print("\nPress key to test network or ESC to exit...")
     numpy.set_printoptions(formatter={'float_kind':"{:.3f}".format})
