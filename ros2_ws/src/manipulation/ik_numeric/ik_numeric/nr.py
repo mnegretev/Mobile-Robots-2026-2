62fdc3418098157ad9b5ef4a3d21a2016d3d4cb7
Irving Rodríguez Ruiz <irving05rdz@gmail.com>
2026-05-19 14:52:06 -0600

    Tarea 10: Cinemática inversa por Newton-Raphson

--- ros2_ws/src/manipulation/ik_numeric/ik_numeric/nr.py
+++ ros2_ws/src/manipulation/ik_numeric/ik_numeric/nr.py
@@ -13,7 +13,7 @@ from manip_msgs.srv import *
 import numpy
 import math
 
-NAME = "FULL NAME"
+NAME = "Irving Rodriguez Ruiz"
 
 H0 = [[1.0, 0.0, 0.0, 0.000], # link1 to link_base, joint rotates on Z
       [0.0, 1.0, 0.0, 0.000],
@@ -87,7 +87,18 @@ class IKNewtonRaphsonNode(Node):
         #     Get RPY from the resulting H
         #     Get xyz from the resulting H
         #
-        x,y,z,R,P,Y = 0,0,0,0,0,0
+        H = numpy.identity(4)
+        for i in range(6):
+            R = numpy.asarray([
+                [numpy.cos(Q[i]), -numpy.sin(Q[i]), 0.0, 0.0],
+                [numpy.sin(Q[i]),  numpy.cos(Q[i]), 0.0, 0.0],
+                [0.0,                          0.0, 1.0, 0.0],
+                [0.0,                          0.0, 0.0, 1.0]
+            ])
+            H = H @ Hs[i] @ R
+        H = H @ Hs[6]
+        R,P,Y = self.matrix_to_euler_xyz(H)
+        x,y,z = H[0,3], H[1,3], H[2,3]
         return numpy.asarray([x, y, z, R, P, Y])
 
     def jacobian(self, Q):
@@ -112,7 +123,11 @@ class IKNewtonRaphsonNode(Node):
         #           i-th column of J = ( FK(i-th row of q_next) - FK(i-th row of q_prev) ) / (2*delta_q)
         #     RETURN J
         #
-        
+        qn = numpy.asarray([Q,]*len(Q)) + numpy.identity(len(Q))*delta_q
+        qp = numpy.asarray([Q,]*len(Q)) - numpy.identity(len(Q))*delta_q
+
+        for i in range(6):
+            J[:, i] = (self.forward_kinematics(qn[i]) - self.forward_kinematics(qp[i]))/(2.0*delta_q)
         return J
         
     def inverse_kinematics(self, Xd, init_guess=numpy.zeros(7), max_iter=2000):
@@ -140,6 +155,22 @@ class IKNewtonRaphsonNode(Node):
         #    Set success if maximum iterations were not exceeded
         #    Return success and calculated Q
         #
+        tol = 0.001
+
+        X = self.forward_kinematics(Q)
+        e = X - Xd
+        e[3:6] = (e[3:6] + math.pi)%(2*math.pi) - math.pi
+
+        while numpy.linalg.norm(e) > tol and iterations < max_iter:
+            J = self.jacobian(Q)
+
+            Q = (Q - numpy.linalg.pinv(J) @ e + math.pi)%(2*math.pi) - math.pi
+
+            X = self.forward_kinematics(Q)
+            e = X - Xd
+            e[3:6] = (e[3:6] + math.pi)%(2*math.pi) - math.pi
+
+            iterations += 1
         
         success = iterations < max_iter
         if success:
@@ -147,7 +178,7 @@ class IKNewtonRaphsonNode(Node):
         else:
             self.get_logger().info("Cannot solve IK")
         return success, Q
-
+    
     def callback_ik_pose2pose(self, req, resp):
         N = self.get_parameter('N').get_parameter_value().integer_value
         Xd = [req.x, req.y, req.z, req.roll, req.pitch, req.yaw]
