db2b597f5c7e1b56ac5fe3fe7165a23a6da65ca3
emmanueldom <emmanueldom007@outlook.com>
2026-06-02 18:43:07 -0600

    new_files_a/m

---
+++ ros2_ws/src/final_project/final_project/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+package_name = 'final_project'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='thedoctor',
+    maintainer_email='marco.negrete@ingenieria.unam.edu',
+    description='TODO: Package description',
+    license='LGPL-3.0-only',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'sm_planner = final_project.sm_planner:main'
+        ],
+    },
+)
